cd704bb2981ce974f96eb061229771adb3a86b4b
1000min <o_ogog@naver.com>
2020-04-15 03:29:25 +0900

    게시물 보기 기능 구현

---
+++ Spring_Toy_Project/src/main/java/com/min/web/board/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.min.web.board.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.min.web.board.model.BoardVO;
+
+public interface BoardService {
+
+		public List<BoardVO> getBoardList() throws Exception;
+
+}
+
+
+
---
+++ Spring_Toy_Project/src/main/java/com/min/web/board/service/BoardServiceImpl.java
@@ -0,0 +1,44 @@
+package com.min.web.board.service;
+
+import java.util.List;
+
+
+
+import javax.inject.Inject;
+
+
+
+import org.springframework.stereotype.Service;
+
+
+
+import com.min.web.board.dao.BoardDAO;
+
+import com.min.web.board.model.BoardVO;
+
+
+
+@Service
+
+public class BoardServiceImpl implements BoardService{
+
+
+
+	@Inject
+
+	private BoardDAO boardDAO;
+
+	
+
+	public List<BoardVO> getBoardList() throws Exception {
+
+		return boardDAO.getBoardList();
+
+	}
+
+
+
+}
+
+
+
