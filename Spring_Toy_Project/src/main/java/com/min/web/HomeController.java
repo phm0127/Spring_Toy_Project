08bf5113ec103b99dd7507e43bed11276aa1c49d
1000min <o_ogog@naver.com>
2020-04-22 00:46:40 +0900

    예외처리

--- Spring_Toy_Project/src/main/java/com/min/web/HomeController.java
+++ Spring_Toy_Project/src/main/java/com/min/web/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,5 +36,19 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	@ExceptionHandler(RuntimeException.class)
+
+	public String exceptionHandler(Model model, Exception e){
+
+	logger.info("exception : " + e.getMessage());
+
+	model.addAttribute("exception", e);
+
+	return "error/exception";
+
+	}
+
+
 	
 }
